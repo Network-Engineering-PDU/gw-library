5d865738e776acb4ffc2d876656b7379cb4d7259
AhmedElghaly <ahmedsaad.2feb@gmail.com>
2026-05-28 19:13:37 +0300

    Add logger for scaned devices

--- ttgwlib/provisioning/prov_manager.py
+++ ttgwlib/provisioning/prov_manager.py
@@ -16,18 +16,23 @@ class ProvManager:
         self.scanning = False
         self.provisioning = False
         self.prov_only_one = False
+        self.scanned_nodes = []
 
     def unprov_handler(self, event):
         if event.event_type == EventType.UNPROV_DISC:
             node = Node(event.data["adv_addr"], event.data["uuid"])
+            if node not in self.scanned_nodes:
+                self.scanned_nodes.append(node)
             # Check if device is stored as provisioned
             if node in self.gw.node_db.get_nodes():
                 self.logger.warning("Provisioned device %s announcing " +
                     "as unprovisioned, removing it", node)
                 self.gw.node_db.remove_node(node)
 
+            self.logger.info("Unprovisioned device discovered: %s (uuid=%s)",
+                    node.mac.hex(), node.uuid.hex())
             if not self.provisioning and self.prov_filter.check(node):
-                self.logger.info("New device %s found", node)
+                self.logger.info("New device %s matched scan filter", node)
                 self.provision(node)
 
     def scan_timeout_handler(self, event):
@@ -39,8 +44,12 @@ class ProvManager:
         self.prov_only_one = one
         if self.scanning or self.gw.is_listener():
             return
+        self.scanned_nodes = []
         self.scanning = True
         self.prov_filter = ScanFilter(uuid_filters, mac_filters)
+
+        self.logger.info("Starting unprovisioned device scan (timeout=%s, one=%s)",
+                timeout, one)
 
         if timeout > 0:
             time_events.ScanTimeout(timeout, self.gw)
@@ -58,6 +67,16 @@ class ProvManager:
         msg = commands.ScanStop()
         self.gw.uart.send_msg(msg.serialize())
         self.gw.remove_event_handler(self.unprov_handler)
+
+        if self.scanned_nodes:
+            devices = ", ".join(
+                f"{node.mac.hex()} (uuid={node.uuid.hex()})"
+                for node in self.scanned_nodes
+            )
+            self.logger.info("Scan stopped. Discovered devices: %s", devices)
+        else:
+            self.logger.info("Scan stopped. No unprovisioned devices discovered.")
+        self.scanned_nodes = []
 
     def provision(self, node):
         if self.provisioning:
